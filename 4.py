643ecb0f5573ebad68953c323a3fb45b97553279
Chris Birkett <chris@cjbirkett.co.uk>
2020-12-04 17:30:21 +0000

    Lambda height check

--- 4.py
+++ 4.py
@@ -16,17 +16,7 @@ def make_passport(line):
 passports = [make_passport(line) for line in passport_lines]
 
 
-def is_valid_height(x):
-    height_cm = height_cm_p.match(x)
-    if height_cm is not None and 150 <= int(height_cm.groups(0)[0]) <= 193:
-        return True
-    height_in = height_in_p.match(x)
-    return height_in is not None and 59 <= int(height_in.groups(0)[0]) <= 76
-
-
 four_digits = re.compile(r"^\d{4}$")
-height_cm_p = re.compile(r"^(\d*)cm$")
-height_in_p = re.compile(r"^(\d*)in$")
 hair_colour = re.compile(r"^#[0-9a-f]{6}")
 eye_colours = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
 pid = re.compile(r"^\d{9}$")
@@ -34,7 +24,7 @@ rules = {
     "byr": lambda x: re.search(four_digits, x) and 1920 <= int(x) <= 2002,
     "iyr": lambda x: re.search(four_digits, x) and 2010 <= int(x) <= 2020,
     "eyr": lambda x: re.search(four_digits, x) and 2020 <= int(x) <= 2030,
-    "hgt": is_valid_height,
+    "hgt": lambda x: (x[-2:] == 'cm' and 150 <= int(x[:-2]) <= 193) or (x[-2:] == 'in' and 59 <= int(x[:-2]) <= 76),
     "hcl": lambda x: re.search(hair_colour, x),
     "ecl": lambda x: x in eye_colours,
     "pid": lambda x: re.search(pid, x)
