4a2af0e5e1c7ec2ed266403563525d106eb41075
Chris Birkett <chris@cjbirkett.co.uk>
2020-12-04 08:54:08 +0000

    Neater day 4

--- 4.py
+++ 4.py
@@ -1,27 +1,19 @@
 import fileinput
 import re
 
-lines = [line.replace('\n', '') for line in fileinput.input()]
-lines.append('')
+lines = [line for line in fileinput.input()]
+passport_lines = [line.replace('\n', ' ').split(' ') for line in ''.join(lines).split('\n\n')]
 
-required_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-optional_fields = ['cid']
 
-four_digits = re.compile(r"^\d\d\d\d$")
-height_cm_p = re.compile(r"^(\d*)cm$")
-height_in_p = re.compile(r"^(\d*)in$")
-hair_colour = re.compile(r"^#[0-9a-f]{6}")
-eye_colours = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
-pid = re.compile(r"^\d{9}$")
-rules = {
-    "byr": lambda x: re.search(four_digits, x) and 1920 <= int(x) <= 2002,
-    "iyr": lambda x: re.search(four_digits, x) and 2010 <= int(x) <= 2020,
-    "eyr": lambda x: re.search(four_digits, x) and 2020 <= int(x) <= 2030,
-    "hgt": lambda x: is_valid_height(x),
-    "hcl": lambda x: re.search(hair_colour, x),
-    "ecl": lambda x: x in eye_colours,
-    "pid": lambda x: re.search(pid, x)
-}
+def make_passport(line):
+    passport = {}
+    for field in line:
+        field_name, field_value = field.split(':')
+        passport[field_name] = field_value
+    return passport
+
+
+passports = [make_passport(line) for line in passport_lines]
 
 
 def is_valid_height(x):
@@ -32,6 +24,24 @@ def is_valid_height(x):
     return height_in is not None and 59 <= int(height_in.groups(0)[0]) <= 76
 
 
+four_digits = re.compile(r"^\d{4}$")
+height_cm_p = re.compile(r"^(\d*)cm$")
+height_in_p = re.compile(r"^(\d*)in$")
+hair_colour = re.compile(r"^#[0-9a-f]{6}")
+eye_colours = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+pid = re.compile(r"^\d{9}$")
+rules = {
+    "byr": lambda x: re.search(four_digits, x) and 1920 <= int(x) <= 2002,
+    "iyr": lambda x: re.search(four_digits, x) and 2010 <= int(x) <= 2020,
+    "eyr": lambda x: re.search(four_digits, x) and 2020 <= int(x) <= 2030,
+    "hgt": is_valid_height,
+    "hcl": lambda x: re.search(hair_colour, x),
+    "ecl": lambda x: x in eye_colours,
+    "pid": lambda x: re.search(pid, x)
+}
+required_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+
+
 def is_valid_passport_1(passport):
     return set(passport.keys()).issuperset(required_fields)
 
@@ -39,29 +49,16 @@ def is_valid_passport_1(passport):
 def is_valid_passport_2(passport):
     if not is_valid_passport_1(passport):
         return False
-    for attribute_name in required_fields:
-        attribute_value = passport[attribute_name]
-        attribute_rule = rules[attribute_name]
-        if not attribute_rule(attribute_value):
+    for field_name in required_fields:
+        field_value = passport[field_name]
+        field_rule = rules[field_name]
+        if not field_rule(field_value):
             return False
     return True
 
 
 def run(passport_checker):
-    current_passport = {}
-    valid_passport_count = 0
-    for line in lines:
-        if line == "":
-            if passport_checker(current_passport):
-                valid_passport_count += 1
-            current_passport = {}
-            continue
-        attributes = line.split(' ')
-        for attribute in attributes:
-            attribute_name, attribute_value = attribute.split(':')
-            current_passport[attribute_name] = attribute_value
-
-    return valid_passport_count
+    return len([1 for passport in passports if passport_checker(passport)])
 
 
 print(run(is_valid_passport_1))
