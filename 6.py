0207e0d4fef73a96d5a0c8eedb7ed25d313c9ba3
Chris Birkett <chris@cjbirkett.co.uk>
2020-12-06 20:28:05 +0000

    Neater day 6

--- 6.py
+++ 6.py
@@ -3,23 +3,14 @@ import fileinput
 lines = [line for line in fileinput.input()]
 group_lines = [line.replace('\n', ' ').split(' ') for line in ''.join(lines).split('\n\n')]
 
+part1 = part2 = 0
 
-def part1():
-    counts = 0
-    for group in group_lines:
-        answers = ''.join(group)
-        distinct_answers = set(answers)
-        counts += len(distinct_answers)
-    return counts
+for group in group_lines:
+    all_answers = ''.join(group)
+    distinct_answers = set(all_answers)
+    shared_answers = set.intersection(*list(map(set, group)))
+    part1 += len(distinct_answers)
+    part2 += len(shared_answers)
 
-
-def part2():
-    counts = 0
-    for group in group_lines:
-        shared_answers = set.intersection(*list(map(set, group)))
-        counts += len(shared_answers)
-    return counts
-
-
-print(part1())
-print(part2())
+print(part1)
+print(part2)
