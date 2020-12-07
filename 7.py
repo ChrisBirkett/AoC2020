22c23a9ca7215bc409771f2ae67b9661985c7b98
Chris Birkett <chris@cjbirkett.co.uk>
2020-12-07 05:31:14 +0000

    Day 7

---
+++ 7.py
@@ -0,0 +1,38 @@
+import fileinput
+
+lines = [line.replace('\n', '').split(' contain ') for line in fileinput.input()]
+rules = {k: v.replace('.', '') for (k, v) in lines}
+
+
+def part1(search_bag):
+    search_bags = [search_bag]
+    result_bags = []
+    while len(search_bags):
+        search_bag = search_bags.pop()
+        for idx, line in enumerate(lines):
+            if search_bag[:-1] in line[1]:
+                search_bags.append(line[0])
+                result_bags.append(line[0])
+    return len(set(result_bags))
+
+
+def get_contents(search_colour):
+    if search_colour[-1:] != 's':
+        search_colour += 's'
+    contents = rules[search_colour].split(', ')
+    number_of_contents = 0
+    for bag in contents:
+        if bag == 'no other bags':
+            continue
+        number = int(bag[:1])
+        colour = bag[2:]
+        number_of_contents += number * (1 + get_contents(colour))
+    return number_of_contents
+
+
+def part2(search_bag):
+    return get_contents(search_bag)
+
+
+print(part1('shiny gold bag'))
+print(part2('shiny gold bag'))
