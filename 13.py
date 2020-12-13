23779e28c27fa4f839a8262c89a1e5774e0a7311
Chris Birkett <chris@cjbirkett.co.uk>
2020-12-13 05:52:13 +0000

    Neater day 13

--- 13.py
+++ 13.py
@@ -2,28 +2,27 @@ import fileinput
 from modint import chinese_remainder
 
 lines = [line.replace('\n', '') for line in fileinput.input()]
+buses = [int(x) if x != 'x' else 'x' for x in lines[1].split(',')]
 
 
 def part1():
     earliest_departure = int(lines[0])
-    buses = [int(x) for x in lines[1].split(',') if x != 'x']
     waits = []
     for bus in buses:
-        num_missed = int(earliest_departure / bus)
-        waits.append((bus, (bus * (num_missed + 1)) % earliest_departure))
+        if bus != 'x':
+            num_missed = int(earliest_departure / bus)
+            waits.append((bus, (bus * (num_missed + 1)) % earliest_departure))
     min_wait = min(waits, key=lambda t: t[1])
     return min_wait[0] * min_wait[1]
 
 
 def part2():
-    buses = [int(x) if x != 'x' else 'x' for x in lines[1].split(',')]
     crt_n = []
     crt_a = []
     for idx, bus in enumerate(buses):
-        if bus == 'x':
-            continue
-        crt_n.append(bus)
-        crt_a.append(bus - idx)
+        if bus != 'x':
+            crt_n.append(bus)
+            crt_a.append(bus - idx)
     return chinese_remainder(crt_n, crt_a)
 
 
