92bbbc828d15ceb5b90bc4b082c2bbefcecc2d29
Chris Birkett <chris@cjbirkett.co.uk>
2020-12-18 06:03:38 +0000

    Neater day 18

--- 18.py
+++ 18.py
@@ -3,30 +3,33 @@ import fileinput
 lines = [line.replace('\n', '') for line in fileinput.input()]
 
 
-def find_matching_parenthesis(chars, pos):
+def find_matching_parenthesis(tokens, pos):
     paren_positions = []
-    for idx, char in enumerate(chars):
+    for idx, token in enumerate(tokens):
         if idx < pos:
             continue
-        if char == '(':
+        if token == '(':
             paren_positions.append(idx)
-        elif char == ')':
+        elif token == ')':
             paren_positions.pop()
             if not len(paren_positions):
                 return idx
 
 
 def calculate(expression, add_first=False):
+    expression = expression.replace('(', '( ').replace(')', ' )')
+    tokens = expression.strip().split(' ')
     while True:
-        paren_pos = expression.find('(')
-        if paren_pos == -1:
+        try:
+            paren_idx = tokens.index('(')
+            closing_paren_idx = find_matching_parenthesis(tokens, paren_idx)
+            subexp = ' '.join(tokens[paren_idx+1:closing_paren_idx])
+            subexp_result = calculate(subexp, add_first)
+            tokens = tokens[:paren_idx] + [str(subexp_result)] + tokens[closing_paren_idx + 1:]
+        except ValueError:
             break
-        closing_paren_pos = find_matching_parenthesis(expression, paren_pos)
-        subexp_result = calculate(expression[paren_pos+1:closing_paren_pos], add_first)
-        expression = expression[:paren_pos] + str(subexp_result) + expression[closing_paren_pos+1:]
 
     if add_first:
-        tokens = expression.split(' ')
         while True:
             try:
                 plus_idx = tokens.index('+')
@@ -34,9 +37,7 @@ def calculate(expression, add_first=False):
                 tokens = tokens[:plus_idx - 1] + [str(result)] + tokens[plus_idx + 2:]
             except ValueError:
                 break
-        expression = ' '.join(tokens)
 
-    tokens = expression.split(' ')
     result = tokens[0]
     current_op = ''
     for token in tokens[1:]:
